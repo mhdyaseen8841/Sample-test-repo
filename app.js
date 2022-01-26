6a56bb7e3eea7f41ce6a663aebdd4114056d5c00
mhdyaseen <yyaseen080@gmail.com>
2022-01-26 16:59:29 +0530

    hbs error solved & header added but not aligned and icon problem

--- app.js
+++ app.js
@@ -12,7 +12,8 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
-app.engine('hbs',hbs({extname:'hbs',defaultLayout:'layout',layoutsDir:__dirname+'/views/layout/',partialsDir:__dirname+'/views/partials/'}))
+app.engine('hbs',hbs.engine({extname:'hbs',defaultLayout:'layout',layoutsDir:__dirname+'/views/layout/',partialsDir:__dirname+'/views/partials/'}))
+
 
 app.use(logger('dev'));
 app.use(express.json());
